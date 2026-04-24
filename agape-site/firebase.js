0e646753a6ad28a2ea8fe081836674b84f1da5f0
sousafabricyoemannoel-tech <sousafabricyoemannoel@gmail.com>
2026-04-23 21:37:02 -0300

    Update firebase.js

--- agape-site/firebase.js
+++ agape-site/firebase.js
@@ -1,24 +1,21 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "COLE_SUA_API_KEY",
-  authDomain: "SEU_PROJETO.firebaseapp.com",
-  projectId: "SEU_ID",
+  apiKey: "AIzaSyADXekMsYObiaFu7NDPLQnaKAtS0gIKXLM",
+  authDomain: "agape-site-5f386.firebaseapp.com",
+  projectId: "agape-site-5f386",
+  storageBucket: "agape-site-5f386.firebasestorage.app",
+  messagingSenderId: "659061483999",
+  appId: "1:659061483999:web:8188d3cd078879c77c3c6a",
+  measurementId: "G-4LDM826BHN"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-
-const provider = new GoogleAuthProvider();
-
-window.loginGoogle = async function () {
-  const result = await signInWithPopup(auth, provider);
-  const user = result.user;
-
-  if (user.email === "sousafabricyoemannoel@gmail.com") {
-    window.location.href = "admin.html";
-  } else {
-    alert("Login realizado!");
-  }
-};
+const analytics = getAnalytics(app);
